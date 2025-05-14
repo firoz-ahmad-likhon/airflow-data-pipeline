c389af34b408e2ff0fdbd6f5618e34fe27f168f3
Likhon <likh.deshi@gmail.com>
2025-05-14 20:34:23 +0600

    Dag test

---
+++ tests/conftest.py
@@ -0,0 +1,67 @@
+import pytest
+from typing import Any
+from collections.abc import Generator
+from model.destination import DestinationPostgreSQL
+
+
+@pytest.fixture(scope="session")
+def mock_data() -> dict[str, list[dict[str, Any]]]:
+    """Fixture to provide mock data for testing."""
+    return {
+        "data": [
+            {
+                "publishTime": "2023-07-21T06:58:08Z",
+                "businessType": "Wind generation",
+                "psrType": "Wind Onshore",
+                "quantity": 640.283,
+                "startTime": "2023-07-21T04:30:00Z",
+                "settlementDate": "2023-07-21",
+                "settlementPeriod": 12,
+            },
+            {
+                "publishTime": "2023-07-21T06:58:08Z",
+                "businessType": "Wind generation",
+                "psrType": "Wind Offshore",
+                "quantity": 77.014,
+                "startTime": "2023-07-21T04:30:00Z",
+                "settlementDate": "2023-07-21",
+                "settlementPeriod": 12,
+            },
+            {
+                "publishTime": "2023-07-21T06:58:08Z",
+                "businessType": "Solar generation",
+                "psrType": "Solar",
+                "quantity": 89,
+                "startTime": "2023-07-21T04:30:00Z",
+                "settlementDate": "2023-07-21",
+                "settlementPeriod": 12,
+            },
+        ],
+    }
+
+
+@pytest.fixture(scope="session")
+def destination() -> Generator[DestinationPostgreSQL, None, None]:
+    """Fixture to instantiate the DestinationPostgreSQL class."""
+    dest = DestinationPostgreSQL()
+
+    # Perform setup actions if needed (e.g., create table)
+    dest.TABLE_NAME = "test_psr"
+
+    dest.query(
+        f"""
+    DROP TABLE IF EXISTS {dest.TABLE_NAME};
+    CREATE TABLE {dest.TABLE_NAME} (
+        curve_name VARCHAR(255),
+        curve_date TIMESTAMP NOT NULL,
+        value NUMERIC,
+        PRIMARY KEY (curve_name, curve_date)
+    );""",
+    )
+
+    # Yield instance for use in tests
+    yield dest
+
+    # Drop the table after the session ends
+    dest.query(f"DROP TABLE IF EXISTS {dest.TABLE_NAME};")
+    dest.disconnect()
---
+++ tests/test_dags/test_integrity_psr_sync.py
@@ -0,0 +1,44 @@
+from .integrity_tester import IntegrityTester
+from airflow.models import DagBag
+
+
+class TestPSRSyncDAG(IntegrityTester):
+    """Test the psr_sync DAG."""
+
+    def test_dag_loaded(self, dag_psr_sync: DagBag) -> None:
+        """Test if the DAG is correctly loaded."""
+        assert DagBag().import_errors == {}, "Improper import"
+        assert dag_psr_sync.id in DagBag().dags, f"DAG '{dag_psr_sync.id}' is missing"
+        assert dag_psr_sync is not None, "DAG object is None"
+        assert len(dag_psr_sync.tasks) > 0, "No tasks in the DAG"
+
+    def test_dag_has_tag(self, dag_psr_sync: DagBag) -> None:
+        """Test if the DAG contains the correct tag."""
+        assert (
+            "half hourly" in dag_psr_sync.tags
+        ), "Tag 'half hourly' is missing in the DAG"
+
+    def test_task_count(self, dag_psr_sync: DagBag) -> None:
+        """Test the number of tasks in the DAG."""
+        expected_task_count = 6
+        assert (
+            len(dag_psr_sync.tasks) == expected_task_count
+        ), f"Expected 5 tasks, but got {len(dag_psr_sync.tasks)}"
+
+    def test_task_dependencies(self, dag_psr_sync: DagBag) -> None:
+        """Test the dependencies between the tasks."""
+        # Define expected upstream and downstream dependencies
+        task_deps = {
+            "Processor.fetch": ["parameterize"],
+            "Processor.validate": ["Processor.fetch"],
+            "Processor.transform": ["Processor.validate"],
+            "sync": ["Processor.transform"],
+        }
+
+        for task_id, upstream_ids in task_deps.items():
+            task = dag_psr_sync.get_task(task_id)
+            assert task is not None, f"Task '{task_id}' is missing in the DAG"
+            upstream_tasks = [t.task_id for t in task.upstream_list]
+            assert set(upstream_ids) == set(
+                upstream_tasks,
+            ), f"Task '{task_id}' has incorrect upstream dependencies"
