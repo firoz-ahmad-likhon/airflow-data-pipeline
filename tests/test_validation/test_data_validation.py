3a9618c95daedc31308932cdc73443150d71b05a
Likhon <likh.deshi@gmail.com>
2025-05-14 20:35:37 +0600

    Validation test

---
+++ tests/test_validation/test_data_validation.py
@@ -0,0 +1,15 @@
+from typing import Any
+from validation.data_validation import DataValidator
+
+
+class TestDataValidator:
+    """Test the parameter validator class."""
+
+    def test_data_validator(self, mock_data: dict[str, list[dict[str, Any]]]) -> None:
+        """Run the validation and check if it passes.
+
+        :param mock_data: Mock data from fixture.
+        """
+        assert DataValidator(
+            mock_data["data"],
+        ).validate(), "Data validation did not pass"
